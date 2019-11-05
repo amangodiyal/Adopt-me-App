943545b8d1f8dc06190b378b9207a9708b944e72
AmanGodiyal <amangodiyal@gmail.com>
2019-11-05 12:51:54 +0530

    Done with basic setup,installed react and separated the components.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import Pet from "./Pet.js";
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!!"),
-    React.createElement(Pet, {
-      name: "Duffy",
-      animal: "Dog",
-      breed: "Labrador"
-    }),
-    React.createElement(Pet, {
-      name: "Heera",
-      animal: "Cat",
-      breed: "Desi"
-    }),
-    React.createElement(Pet, {
-      name: "Bubli",
-      animal: "Cow",
-      breed: "BJP"
-    })
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!!"),
+  //   React.createElement(Pet, {
+  //     name: "Duffy",
+  //     animal: "Dog",
+  //     breed: "Labrador"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Heera",
+  //     animal: "Cat",
+  //     breed: "Desi"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Bubli",
+  //     animal: "Cow",
+  //     breed: "BJP"
+  //   })
+  // ]);
+  return (
+    <div>
+      <h1>"Adopt Me!!"</h1>
+      <Pet name="Duffy" animal="Dog" breed="Labrador" />
+      <Pet name="Meo" animal="Cat" breed="Hawanese" />
+      <Pet name="Sitara" animal="Bird" breed="Mohogini" />
+    </div>
+  );
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++
@@ -1,7 +0,0 @@
-export default Pet = function({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
